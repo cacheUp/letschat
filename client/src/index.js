10d281b8bcdf17238e3c38ebf43a8fb8845e0e1f
cacheUp <bradb26@gmail.com>
2019-07-31 09:34:06 +0300

    auth init

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+const Root = () => (
+  <Router>
+    <Switch>
+      <Route path="/" component={App} />
+    </Switch>
+  </Router>
+);
 
 ReactDOM.render(<App />, document.getElementById("root"));
