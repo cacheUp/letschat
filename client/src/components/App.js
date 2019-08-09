29c14c71da295c52387f98beda6c82e57e086958
cacheUp <bradb26@gmail.com>
2019-08-09 08:43:30 +0300

    added Messages, ColorPanel, MetaPanel

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Grid } from "semantic-ui-react";
+import ColorPanel from "../components/ColorPanel/ColorPanel";
+import SidePanel from "../components/SidePanel/SidePanel";
+import Messages from "../components/Messages/Messages";
+import MetaPanel from "../components/MetaPanel/MetaPanel";
 
 function App() {
-  return <div>react</div>;
+  return (
+    <Grid>
+      <ColorPanel />
+      <SidePanel />
+      <Messages />
+      <MetaPanel />
+    </Grid>
+  );
 }
 
 export default App;
