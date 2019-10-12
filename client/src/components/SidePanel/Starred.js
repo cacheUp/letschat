56e09f53f8c7b24c6a2be9fe7dd1668506704cc7
cacheUp <bradb26@gmail.com>
2019-10-12 18:42:47 +0300

    starred channel

--- client/src/components/SidePanel/Starred.js
+++ client/src/components/SidePanel/Starred.js
@@ -1,10 +1,38 @@
 import React from "react";
+import { connect } from "react-redux";
+import { setCurrentChannel, setPrivateChannel } from "../../actions/index";
+
 import { Menu, Icon } from "semantic-ui-react";
 
 class Starred extends React.Component {
   state = {
+    activeChannel: "",
     starredChannels: []
   };
+
+  setActiveChannel = channel => {
+    this.setState({ activeChannel: channel.id });
+  };
+
+  changeChannel = channel => {
+    this.setActiveChannel(channel);
+    this.props.setCurrentChannel(channel);
+    this.props.setPrivateChannel(false);
+  };
+
+  displayChannels = starredChannels =>
+    starredChannels.length > 0 &&
+    starredChannels.map(channel => (
+      <Menu.Item
+        key={channel.id}
+        onClick={() => this.changeChannel(channel)}
+        name={channel.name}
+        style={{ opacity: 0.7 }}
+        active={channel.id === this.state.activeChannel}
+      >
+        # {channel.name}
+      </Menu.Item>
+    ));
   render() {
     const { starredChannels } = this.state;
     return (
@@ -21,4 +49,7 @@ class Starred extends React.Component {
     );
   }
 }
-export default Starred;
+export default connect(
+  null,
+  { setCurrentChannel, setPrivateChannel }
+)(Starred);
