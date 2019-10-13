2f5f15484c6764c7658a529c48b0196f0b849eb2
cacheUp <bradb26@gmail.com>
2019-10-13 12:40:33 +0300

    changed path for firebase rules

--- client/src/components/SidePanel/UserPannel.js
+++ client/src/components/SidePanel/UserPannel.js
@@ -57,7 +57,7 @@ class UserPanel extends Component {
   uploadCroppedImage = () => {
     const { storageRef, userRef, blob, metadata } = this.state;
     storageRef
-      .child(`avatar/user-${userRef.uid}`)
+      .child(`avatar/user/${userRef.uid}`)
       .put(blob, metadata)
       .then(snap => {
         snap.ref.getDownloadURL().then(downloadURL => {
